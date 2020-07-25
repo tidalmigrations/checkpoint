362513a73059387c4a172f89d729a6e5327481fe
Petr Shevtsov <petr.shevtsov@gmail.com>
2020-07-26 00:15:26 +0300

    SIGNIT, EOF, data file
    Exit on SIGINT instead of any signal
    Exit when reach EOF
    Read and write checkpoint data from/to a file
    Skip the amount of lines read from data file

--- main.go
+++ main.go
@@ -2,18 +2,46 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"sync/atomic"
 )
 
 func main() {
-	var i uint64
+	var (
+		fileName   string
+		skip       uint64
+		checkpoint uint64
+		canPrint   bool
+	)
+
+	flag.StringVar(&fileName, "file", "checkpoint_data", "checkpoint data file")
+	flag.StringVar(&fileName, "f", "checkpoint_data", "checkpoint data file")
+
+	flag.Parse()
+
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil{
+		log.Print(err)
+        content = []byte("0")
+	}
+
+	skip, err = strconv.ParseUint(string(content), 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if skip == 0 {
+		canPrint = true
+	}
 
 	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh)
+	signal.Notify(signalCh, os.Interrupt)
 
 	lineCh := make(chan string)
 	go func() {
@@ -21,6 +49,7 @@ func main() {
 		for scanner.Scan() {
 			lineCh <- scanner.Text()
 		}
+		close(lineCh)
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
@@ -30,12 +59,24 @@ func main() {
 		select {
 		case s := <-signalCh:
 			log.Println("Got signal:", s)
-			final := atomic.LoadUint64(&i)
+			final := atomic.LoadUint64(&checkpoint)
 			log.Println("Checkpoint:", final)
+            content := []byte(strconv.FormatUint(final, 10))
+			if err := ioutil.WriteFile(fileName, content, 0644); err != nil {
+				log.Fatal(err)
+			}
 			os.Exit(0)
-		case line := <-lineCh:
-			fmt.Println(line)
-			atomic.AddUint64(&i, 1)
+		case line, more := <-lineCh:
+			if !more {
+				os.Exit(0)
+			}
+			if canPrint {
+				fmt.Println(line)
+			}
+			current := atomic.AddUint64(&checkpoint, 1)
+			if !canPrint && current > skip {
+				canPrint = true
+			}
 		}
 	}
 }
